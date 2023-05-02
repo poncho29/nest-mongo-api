5e519ee4de91114f55058f9323e75ef44f7c888b
poncho29 <meneses0828@gmail.com>
2023-05-01 20:26:25 -0500

    [add] - seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokeResponse } from './interfaces/poke-response';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed execute';
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    // Opción 1. Crear varias insercciones a la bd
+    // const insertPromiseArray = [];
+
+    // data.results.forEach(({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+
+    //   await this.pokemonModel.create({ name, no });
+    //   insertPromiseArray.push(this.pokemonModel.create({ name, no }));
+    // });
+
+    // await Promise.all(insertPromiseArray);
+
+    // Opción 2: Solo hace un insección a la bd
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      pokemonToInsert.push({ name, no });
+    });
+
+    this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
   }
 }
